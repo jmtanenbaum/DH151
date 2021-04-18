94f419ed15824a8e5ebe51eecebcd9a9f154c7de
Julia <julia.tanenbaum@gmail.com>
2021-04-17 20:31:47 -0700

    add images

--- week3/map.js
+++ week3/map.js
@@ -4,31 +4,37 @@ let bookstores = [
 'title':'Los Angeles',
 'description': '<br> The Last Bookstore is a gigantic new and used independent bookstore. The 22,000 sq. ft. space is in a multistory building in the Spring Arts Tower at 5th & Spring. One of my favorite features is a tunnel made of books.',
 'lat': 34.04798744765223,
-'lon': -118.24957431591702
+'lon': -118.24957431591702,
+'url': 'LA.jpeg'
 },
 {
 'title':'Philadelphia',
 'description': '<br> The Wooden Shoe is an anarchist book and record shop and meeting space in South Philadelphia. I’ve spent many hours watching speakers there and have volunteered a few times. It is an incredible space for the local activist community.',
 'lat': 39.942297116576626,
-'lon': -75.15415484461005
+'lon': -75.15415484461005,
+'url': 'philly.jpeg'
+
 },
 {
 'title':'Portland',
 'description': '<br> Powells Books is the largest independent bookstore I’ve ever visited, and has locations all over Portland. Last time I visited Portland I returned with an entire suitcase of books.',
 'lat': 45.523280700180955,
-'lon': -122.68122634445496
+'lon': -122.68122634445496,
+'url': 'portland.jpeg'
 },
 {
 'title':'New York City',
 'description': '<br> Bluestockings is an old radical feminist bookshop on the Lower East Side of New York City. It is well known for hosting events and also includes a small café. I visited while conducting research at local archives during my undergraduate degree.',
 'lat': 40.71913576868951,
-'lon': -73.98599350410622
+'lon': -73.98599350410622,
+'url': 'nyc.jpeg'
 },
 {
 'title':'Baltimore',
 'description':'<br> Red Emmas Bookstore is another anarchist infoshop, but this one is located in Baltimore. They have played a crucial role in the movement for Black lives in the city, and provide a space for activists to gather and rest.',
 'lat': 39.30437080981874,
-'lon': -76.61830814462655
+'lon': -76.61830814462655,
+'url': 'baltimore.jpeg'
 }
 ]
 
@@ -53,9 +59,9 @@ let myMarkers = L.featureGroup();
 // loop through data
 bookstores.forEach(function(item,index){
 	// create marker
-	 marker = L.marker([item.lat,item.lon], {icon: book}) 
+	 marker = L.marker([item.lat,item.lon,], {icon: book}) 
 
-		.bindPopup(item.title + item.description)
+	 .bindPopup(`<div><h2>${item.title}</h2></div><br>${item.description}</br><img src="${item.url}" width=80%>`)
     // add data to sidebar with onclick event
     $('.sidebar').append(`<div class="sidebar-item" onclick="flyToIndex(${index})">${item.title}</div>`)
 
