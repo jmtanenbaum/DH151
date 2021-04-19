ae25b53eb9c9cf469ff3a79a39678581a1285029
Julia <julia.tanenbaum@gmail.com>
2021-04-19 16:17:44 -0700

    edit

--- week4/js/map.js
+++ week4/js/map.js
@@ -1,10 +1,9 @@
-// global variables 
+// Global variables
 let map;
 let lat = 0;
 let lon = 0;
 let zl = 3;
-// path to csv data
-let path = "data/dunitz.csv"; 
+let path = "data/dunitz.csv";
 let markers = L.featureGroup();
 
 // initialize
@@ -21,6 +20,7 @@ function createMap(lat,lon,zl){
 		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
 }
+
 // function to read csv data
 function readCSV(path){
 	Papa.parse(path, {
@@ -35,20 +35,41 @@ function readCSV(path){
 		}
 	});
 }
+
 function mapCSV(data){
-	
+
+	// circle options
+	let circleOptions = {
+		radius: 5,
+		weight: 1,
+		color: 'white',
+		fillColor: 'dodgerblue',
+		fillOpacity: 1
+	}
+
 	// loop through each entry
 	data.data.forEach(function(item,index){
-		// create marker
-		let marker = L.marker([item.latitude,item.longitude])
+		// create a marker
+		let marker = L.circleMarker([item.latitude,item.longitude],circleOptions)
+		.on('mouseover',function(){
+			this.bindPopup(`${item.title}<br><img src="${item.thumbnail_url}">`).openPopup()
+		})
 
 		// add marker to featuregroup
 		markers.addLayer(marker)
+
+		// add entry to sidebar
+		$('.sidebar').append(`<img src="${item.thumbnail_url}" onmouseover="panToImage(${index})">`)
 	})
 
 	// add featuregroup to map
 	markers.addTo(map)
 
-	// fit markers to map
-	map.fitBounds(markers.getBounds()); 
+	// fit map to markers
+	map.fitBounds(markers.getBounds())
+}
+
+function panToImage(index){
+	map.setZoom(17);
+	map.panTo(markers.getLayers()[index]._latlng);
 }
