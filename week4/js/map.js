eb334e770ee7f7158d27adca50a78bd81568ba23
Julia <julia.tanenbaum@gmail.com>
2021-04-19 15:59:08 -0700

    commit

--- week4/js/map.js
+++ week4/js/map.js
@@ -1,11 +1,16 @@
+// global variables 
 let map;
 let lat = 0;
 let lon = 0;
 let zl = 3;
+// path to csv data
+let path = "data/dunitz.csv"; 
+let markers = L.featureGroup();
 
 // initialize
 $( document ).ready(function() {
-    createMap(lat,lon,zl);
+	createMap(lat,lon,zl);
+	readCSV(path);
 });
 
 // create the map
@@ -15,4 +20,35 @@ function createMap(lat,lon,zl){
 	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
+}
+// function to read csv data
+function readCSV(path){
+	Papa.parse(path, {
+		header: true,
+		download: true,
+		complete: function(data) {
+			console.log(data);
+			
+			// map the data
+			mapCSV(data);
+
+		}
+	});
+}
+function mapCSV(data){
+	
+	// loop through each entry
+	data.data.forEach(function(item,index){
+		// create marker
+		let marker = L.marker([item.latitude,item.longitude])
+
+		// add marker to featuregroup
+		markers.addLayer(marker)
+	})
+
+	// add featuregroup to map
+	markers.addTo(map)
+
+	// fit markers to map
+	map.fitBounds(markers.getBounds()); 
 }
