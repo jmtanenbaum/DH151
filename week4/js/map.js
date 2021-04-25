37a8a5f5967aaaa1969d4e54f0f56b733cf53448
Julia <julia.tanenbaum@gmail.com>
2021-04-24 22:21:18 -0700

    map

--- week4/js/map.js
+++ week4/js/map.js
@@ -3,7 +3,7 @@ let map;
 let lat = 0;
 let lon = 0;
 let zl = 3;
-let path = "data/dunitz.csv";
+let path = "data/covid.csv";
 let markers = L.featureGroup();
 
 // initialize
@@ -14,7 +14,7 @@ $( document ).ready(function() {
 
 // create the map
 function createMap(lat,lon,zl){
-	map = L.map('map').setView([lat,lon], zl);
+	map = L.map('map').setView([Lat,Long], zl);
 
 	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -50,16 +50,16 @@ function mapCSV(data){
 	// loop through each entry
 	data.data.forEach(function(item,index){
 		// create a marker
-		let marker = L.circleMarker([item.latitude,item.longitude],circleOptions)
+		let marker = L.circleMarker([item.Lat,item.Long],circleOptions)
 		.on('mouseover',function(){
-			this.bindPopup(`${item.title}<br><img src="${item.thumbnail_url}">`).openPopup()
+			this.bindPopup(`${item.Facility}`).openPopup()
 		})
 
 		// add marker to featuregroup
 		markers.addLayer(marker)
 
 		// add entry to sidebar
-		$('.sidebar').append(`<img src="${item.thumbnail_url}" onmouseover="panToImage(${index})">`)
+		//$('.sidebar').append(`<img src="${item.thumbnail_url}" onmouseover="panToImage(${index})">`)
 	})
 
 	// add featuregroup to map
